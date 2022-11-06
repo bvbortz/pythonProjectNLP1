7cd5d01c0b82a1d23243bdd203787af19276fd15
bvbortz <Barry.Bortz@mail.huji.ac.il>
2022-11-06 15:55:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import spacy
+import numpy as np
+from datasets import load_dataset
+from collections import Counter
+
+def bigram(doc):
+    result = list()
+    sentence = list()
+
+    for token in doc:
+        if token.is_alpha:
+            sentence.append(token)
+
+    for wold in range(len(sentence) - 1):
+        first_word = sentence[wold]
+        second_word = sentence[wold + 1]
+        elemnt = [first_word, second_word]
+        result.append(elemnt)
+
+    return result
+
+def unigram(dataset):
+    cnt = Counter()
+    sentence = list()
+    length = 0
+    for text in dataset['text']:
+        doc = nlp(text)
+        for token in doc:
+            if token.is_alpha:
+                # sentence.append(token)
+                length += 1
+                cnt[token.lemma_] += 1
+                if length % 100 == 0:
+                    print(length)
+    # for word in sentence:
+    #     cnt[word] += 1
+    return lambda x: np.log(cnt[x] / length)
+
+
+def get_probability_unigram(sentence, lambda_model):
+    p = 1
+    for word in sentence:
+        p *= lambda_model(word)
+    return p
+
+
+nlp = spacy.load("en_core_web_sm")
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
+dod_test = nlp("i want to make a cake ")
+lambda_model_unigram = unigram(dataset)
+print(get_probability_unigram(nlp("I have a house in the US"), lambda_model_unigram))
