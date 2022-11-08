6e2b019e5683b6bd58a5e4d25b824d970246faea
bvbortz <Barry.Bortz@mail.huji.ac.il>
2022-11-08 14:00:30 +0200

    bigram added

--- main.py
+++ main.py
@@ -3,26 +3,48 @@ import numpy as np
 from datasets import load_dataset
 from collections import Counter
 
-def bigram(doc):
+def bigram(dataset):
     result = list()
-    sentence = list()
-
-    for token in doc:
-        if token.is_alpha:
-            sentence.append(token)
-
-    for wold in range(len(sentence) - 1):
-        first_word = sentence[wold]
-        second_word = sentence[wold + 1]
-        elemnt = [first_word, second_word]
-        result.append(elemnt)
-
-    return result
-
-def unigram(dataset):
-    cnt = Counter()
+    cnt_dict_tuples = dict()
     cnt_dict = dict()
     sentence = list()
+    length = 0
+    for text in dataset['text']:
+        doc = nlp(text)
+        cnt = 0
+        i = 0
+        while i < len(doc)-1:
+            if doc[i].is_alpha:
+                length += 1
+                if length % 100 == 0:
+                    print(length)
+                if cnt == 0:
+                    cnt += 1
+                    cur_key = ('START', doc[i].lemma_)
+                    update_dict(cnt_dict_tuples, cur_key)
+                    update_dict(cnt_dict, 'START')
+                else:
+                    tmp = i
+                    while i < len(doc)-1 and not doc[i+1].is_alpha:
+                        i += 1
+                    if i >= len(doc)-1:
+                        break
+                    cur_key = (doc[tmp].lemma_, doc[i+1].lemma_)
+                    update_dict(cnt_dict_tuples, cur_key)
+                    update_dict(cnt_dict, doc[tmp].lemma_)
+                i += 1
+    return cnt_dict_tuples, cnt_dict, length
+
+
+def update_dict(cnt_dict, cur_key):
+    if cur_key in cnt_dict:
+        cnt_dict[cur_key] += 1
+    else:
+        cnt_dict[cur_key] = 1
+
+
+def unigram(dataset):
+    cnt_dict = dict()
     length = 0
     for text in dataset['text']:
         doc = nlp(text)
@@ -30,11 +52,7 @@ def unigram(dataset):
             if token.is_alpha:
                 # sentence.append(token)
                 length += 1
-                if token.lemma_ in cnt_dict:
-                    cnt_dict[token.lemma_] += 1
-                else:
-                    cnt_dict[token.lemma_] = 1
-                cnt[token.lemma_] += 1
+                update_dict(cnt_dict, token.lemma_)
                 if length % 100 == 0:
                     print(length)
     # for word in sentence:
@@ -47,12 +65,33 @@ def get_probability_unigram(sentence, cnt_dict, length):
     for token in sentence:
         if token.is_alpha:
             cnt = cnt_dict[token.lemma_]
-            p *= np.log(cnt / length)
+            p += np.log(cnt / length)
+    return p
+
+
+def get_probability_bigram(sentence, cnt_dict, cnt_dict_tuples, length):
+    p = 1
+    cnt = 0
+    i = 0
+    while i < len(sentence) - 1:
+        if sentence[i].is_alpha:
+            if cnt == 0:
+                cnt += 1
+                cur_key = ('START', sentence[i].lemma_)
+                p *= cnt_dict_tuples[cur_key] / cnt_dict['START']
+            else:
+                tmp = i
+                while i < len(sentence) - 1 and not sentence[i + 1].is_alpha:
+                    i += 1
+                if i >= len(sentence) - 1:
+                    break
+                cur_key = (sentence[tmp].lemma_, sentence[i + 1].lemma_)
+                p += np.log(cnt_dict_tuples[cur_key] / sentence[tmp].lemma_)
+        i += 1
     return p
 
 
 nlp = spacy.load("en_core_web_sm")
 dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
-dod_test = nlp("i want to make a cake ")
-cnt_dict, length = unigram(dataset)
-print(get_probability_unigram(nlp("I have a house in the US"), cnt_dict, length))
+cnt_dict_tuples, cnt_dict, length = bigram(dataset)
+print(get_probability_bigram(nlp("I have a house in the"), cnt_dict, cnt_dict_tuples, length))
